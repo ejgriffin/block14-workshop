cfc77cb14fb70f5ad739856e3d187a95ee67655f
Eric Griffin <141835816+ejgriffin@users.noreply.github.com>
2023-09-28 17:56:46 -0400

    Add files via upload
    second attempt

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//I did not get to this.
+//Only odds.
+//Create a JavaScript code that accepts an array of numbers and returns a new array with only the odd numbers from the original array.
+//If there is only one odd number in the array, return an array with that single value.
+
+const inputArr1 = [2, 4, 6, 8, 11, 20, 15, 22];
+console.log(inputArr1.slice(0, 1, 2, 3, 4, 6, 8));
